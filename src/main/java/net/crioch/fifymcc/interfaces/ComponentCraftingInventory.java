90ab7c2ec4b93fdbeab3565cc6c39b41d9a5f3a1
CriochDev <crioch@proton.me>
2024-06-04 11:31:23 -0600

    Injected Interfaces failing in prod
    Replaced with duck interfaces

--- src/main/java/net/crioch/fifymcc/interfaces/ComponentCraftingInventory.java
+++
@@ -1,7 +0,0 @@
-package net.crioch.fifymcc.interfaces;
-
-import net.crioch.fifymcc.recipe.ComponentRecipeMatcher;
-
-public interface ComponentCraftingInventory {
-    default void provideComponentRecipeInputs(ComponentRecipeMatcher finder) {}
-}
