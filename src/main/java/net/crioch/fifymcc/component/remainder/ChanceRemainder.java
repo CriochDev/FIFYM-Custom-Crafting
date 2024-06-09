8de694815ea77982c4f373bc8b76ed9ffcd94e62
CriochDev <crioch@proton.me>
2024-06-08 23:43:37 -0600

    Fixed naming conventions within components

--- src/main/java/net/crioch/fifymcc/component/remainder/ChanceRemainder.java
+++ src/main/java/net/crioch/fifymcc/component/remainder/ChanceRemainder.java
@@ -3,28 +3,27 @@ package net.crioch.fifymcc.component.remainder;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import net.crioch.fifymcc.interfaces.RandomGetSeed;
 import net.crioch.fifymcc.util.FIFYCodecs;
 import net.crioch.fifymcc.util.Util;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
-public class BreakableRemainder extends RemainderWithSeed {
-    public static final Identifier ID = new Identifier(Util.MOD_ID, "breakable_remainder");
+public class ChanceRemainder extends RemainderWithSeed {
+    public static final Identifier ID = new Identifier(Util.MOD_ID, "chance_remainder");
 
-    public static final MapCodec<BreakableRemainder> CODEC = (RecordCodecBuilder.mapCodec(
+    public static final MapCodec<ChanceRemainder> CODEC = (RecordCodecBuilder.mapCodec(
             instance -> instance.group(
-                    FIFYCodecs.FLOAT_CHANCE.fieldOf("break_chance").forGetter(BreakableRemainder::chance),
-                    Codec.LONG.optionalFieldOf("seed", 0L).forGetter(BreakableRemainder::getSeed)
-            ).apply(instance, BreakableRemainder::new)
+                    FIFYCodecs.FLOAT_CHANCE.fieldOf("break_chance").forGetter(ChanceRemainder::chance),
+                    Codec.LONG.optionalFieldOf("seed", 0L).forGetter(ChanceRemainder::getSeed)
+            ).apply(instance, ChanceRemainder::new)
     ));
     private final float chance;
     public float chance() {
         return this.chance;
     }
 
-    public BreakableRemainder(float chance, long seed) {
+    public ChanceRemainder(float chance, long seed) {
         super(RemainderTypes.BREAKABLE, seed);
         this.chance = chance;
     }
--- src/main/java/net/crioch/fifymcc/recipe/FIFYMRecipeMatcherHelper.java
+++ src/main/java/net/crioch/fifymcc/recipe/FIFYMRecipeMatcherHelper.java
@@ -1,15 +1,13 @@
 package net.crioch.fifymcc.recipe;
 
 import net.crioch.fifymcc.component.FIFYDataComponentTypes;
-import net.crioch.fifymcc.component.remainder.DamageableRemainder;
-import net.crioch.fifymcc.component.remainder.Remainder;
+import net.crioch.fifymcc.component.remainder.DamagedRemainder;
 import net.minecraft.component.ComponentChanges;
 import net.minecraft.component.DataComponentType;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.ItemStack;
 
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 
 public class FIFYMRecipeMatcherHelper {
@@ -32,7 +30,7 @@ public class FIFYMRecipeMatcherHelper {
 
     public static ComponentChanges removeIgnoredChanges(ItemStack stack) {
         ComponentChanges changes = stack.getComponentChanges();
-        if (stack.get(FIFYDataComponentTypes.RECIPE_REMAINDER) instanceof DamageableRemainder remainder) {
+        if (stack.get(FIFYDataComponentTypes.RECIPE_REMAINDER) instanceof DamagedRemainder remainder) {
             if ((remainder.canBreak() || stack.getDamage() < stack.getMaxDamage())) {
                 changes = changes.withRemovedIf(dataComponentType -> dataComponentType.equals(DataComponentTypes.DAMAGE));
             }
