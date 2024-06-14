3da8d28dc1817306e3f5f6a1d24689dc6f09340b
CriochDev <crioch@proton.me>
2024-06-13 18:40:23 -0600

    Enchantments working again

--- src/main/java/net/crioch/fifymcc/mixin/recipe/RecipeMixin.java
+++ src/main/java/net/crioch/fifymcc/mixin/recipe/RecipeMixin.java
@@ -1,6 +1,7 @@
 package net.crioch.fifymcc.mixin.recipe;
 
 import net.crioch.fifymcc.component.FIFYMDataComponentTypes;
+import net.crioch.fifymcc.component.remainder.RecipeRemainderWithWorld;
 import net.crioch.fifymcc.component.remainder.Remainder;
 import net.minecraft.component.ComponentMap;
 import net.minecraft.inventory.Inventory;
@@ -9,13 +10,14 @@ import net.minecraft.recipe.Recipe;
 import net.minecraft.recipe.RepairItemRecipe;
 import net.minecraft.recipe.input.RecipeInput;
 import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Recipe.class)
-public interface RecipeMixin<C extends RecipeInput> {
+public interface RecipeMixin<C extends RecipeInput> extends RecipeRemainderWithWorld<C> {
     @Inject(method = "getRemainder", at = @At("HEAD"), cancellable = true)
     private void replaceGetRecipeRemainder(C input, CallbackInfoReturnable<DefaultedList<ItemStack>> cir) {
 
@@ -30,5 +32,20 @@ public interface RecipeMixin<C extends RecipeInput> {
         }
 
         cir.setReturnValue(defaultedList);
+    }
+
+    @Override
+    default DefaultedList<ItemStack> fIFYM_CustomCrafting$getRemainder(C input, World world) {
+        DefaultedList<ItemStack> defaultedList = DefaultedList.ofSize(input.getSize(), ItemStack.EMPTY);
+
+        for(int i = 0; i < defaultedList.size(); ++i) {
+            ItemStack stack = input.getStackInSlot(i);
+            Remainder remainder = stack.get(FIFYMDataComponentTypes.RECIPE_REMAINDER);
+            if (remainder != null && !(this instanceof RepairItemRecipe)) {
+                defaultedList.set(i, remainder.getRemainder(stack, world));
+            }
+        }
+
+        return defaultedList;
     }
 }
