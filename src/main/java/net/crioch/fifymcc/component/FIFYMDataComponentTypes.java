fb0669dadb0be2b8eebaae49e08e98067227afc2
CriochDev <crioch@proton.me>
2024-06-14 12:31:28 -0600

    Updated README.md and changes brewing fuel component name

--- src/main/java/net/crioch/fifymcc/component/FIFYMDataComponentTypes.java
+++ src/main/java/net/crioch/fifymcc/component/FIFYMDataComponentTypes.java
@@ -25,7 +25,7 @@ public class FIFYMDataComponentTypes {
 
     public static void register() {
         // Simple components
-        BREWING_STAND_FUEL = FIFYMDataComponentTypes.register("brewing_stand_fuel", builder -> builder.codec(Codecs.rangedInt(1, 20)).packetCodec(PacketCodecs.VAR_INT));
+        BREWING_STAND_FUEL = FIFYMDataComponentTypes.register("brewing_fuel", builder -> builder.codec(Codecs.rangedInt(1, 20)).packetCodec(PacketCodecs.VAR_INT));
         COMPOST_CHANCE = FIFYMDataComponentTypes.register("compost_chance", builder -> builder.codec(FIFYMCodecs.FLOAT_CHANCE).packetCodec(PacketCodecs.FLOAT));
         ENCHANTABILITY = FIFYMDataComponentTypes.register("enchantability", builder -> builder.codec(Codecs.NONNEGATIVE_INT).packetCodec(PacketCodecs.VAR_INT));
         FUEL_VALUE = FIFYMDataComponentTypes.register("fuel_value", builder -> builder.codec(Codecs.POSITIVE_INT).packetCodec(PacketCodecs.VAR_INT));
